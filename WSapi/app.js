efe6e2bb9fca676558c328296e5ac09d71493f27
stefanotolomeo <ste.tolomeo@gmail.com>
2018-04-09 11:22:44 +0200

    redirect

--- WSapi/app.js
+++ WSapi/app.js
@@ -19,7 +19,4 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = serverConfig.port;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
 });
--- db/config.js
+++ db/config.js
@@ -1,12 +1,13 @@
 
 module.exports = {
-    'url' : 'mongodb://localhost:27017',
+    //'url' : 'mongodb://localhost:27017',
+    'url' : 'mongodb://54.93.97.25:27017',
     'measurements' : '/polimi_measurements',
     'indicators' : '/polimi_indicators',
-    'SUM' : '/messages',         // Single User Message
-    'SUC' : '/calls',            // Single User Call
-    'SSU' : '/smartphoneUse',    // Summary Smartphone Use
-    'PSU' : '/peopleUse',         // People Smartphone Use
+    'SUM' : '/messages',         // Single User Message collection
+    'SUC' : '/calls',            // Single User Call collection
+    'SSU' : '/smartphoneUse',    // Summary Smartphone Use collection
+    'PSU' : '/peopleUse',         // People Smartphone Use collection
     'type': ['IN', 'OUT'],
     'temporality': ['timestamp', 'datestamp', 'timeinterval']
 };
