a3e0f22fdda872697c0005e09ba3bd641dfbaf3c
stefanotolomeo <ste.tolomeo@gmail.com>
2018-04-12 20:42:56 +0200

    official release

--- gaitWS/api/controllers/uploadFileCtrl.js
+++ gaitWS/api/controllers/uploadFileCtrl.js
@@ -3,20 +3,55 @@
 var util = require('util');
 
 var fs = require('fs');
+var formidable = require('formidable');
+var delayedStream = require('delayed-stream');
 
-var commonFunctions = require("./commonFunctions");
 
 module.exports = {
   uploadFile: uploadGaitFile
 };
 
+
+
 function uploadGaitFile (req, res){
     // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
     console.log(" --> UploadGaitFile : START");
-    console.log(" --> Req URL : "+req.url);
-    console.log(' --> Req Method: %s', req.method);
+    var delayed = delayedStream.create(req);
+    delayed.pause();
 
+    var form = new formidable.IncomingForm();
+    console.log(' --> Formidable initialized');
+    if(form == null){
+        console.log(' --> Formidable is NULL');
+    } else{
+        console.log(' --> Not NULL');
+    }
+    delayed.resume();
+    form.parse(delayed.source);
+    /*form.parse(req, function (err, fields, files) {
+      res.json({success:true});
+    });*/
 
+    /*form.once('error', console.log)
+    form.parse(req, function (err, fields, files) {})*/
+    /*form.parse(req, function (err, fields, files) {
+      res.json(true);
+    });
+*/
 
 }
+
+/*
+if (err){
+            console.log("---> ERROR <--- !");
+            res.json({
+                success:false,
+                error: err.message
+            });
+        }
+        else{
+            console.log("CALL stored");
+            res.json({success:true});
+        }
+        */
 
